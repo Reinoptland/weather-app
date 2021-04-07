f68d642192c1cc077c4f897f8b9b3f14358d0335
Reinoptland <reinoptland@gmail.com>
2021-04-07 13:32:10 +0200

    Write plan

---
+++ Steps.md
@@ -0,0 +1,33 @@
+# Fetching
+
+## Fetching data onclick
+
+1. Button maken
+2. Event listener & handler toevoegen
+3. axios installeren
+4. axios importeren
+5. async functie
+6. axios.get
+7. response checken
+8. state aanmaken
+9. response opslaan in de state
+10. response laten zien (JSX)
+
+## Fetching data (in React, on mount)
+
+### Happy path
+
+1. axios installeren
+2. axios importeren
+3. useEffect
+4. async functie
+5. axios.get
+6. response checken
+7. state aanmaken
+8. response opslaan in de state
+9. response laten zien (JSX)
+
+### Unhappy path
+
+1. try catch
+2. error handling
--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React from 'react';
-import SearchBar from './components/searchBar/SearchBar';
-import TabBarMenu from './components/tabBarMenu/TabBarMenu';
-import MetricSlider from './components/metricSlider/MetricSlider';
-import './App.css';
+import React from "react";
+import SearchBar from "./components/searchBar/SearchBar";
+import TabBarMenu from "./components/tabBarMenu/TabBarMenu";
+import MetricSlider from "./components/metricSlider/MetricSlider";
+import "./App.css";
+
+const apiKey = "0ac4c9dcc5a66ea3d2a9fe36e3bb7761";
 
 function App() {
   return (
     <>
       <div className="weather-container">
-
         {/*HEADER -------------------- */}
         <div className="weather-header">
-          <SearchBar/>
+          <SearchBar />
 
           <span className="location-details">
             <h2>Bewolkt</h2>
             <h3> </h3>
             <h1>14 &deg;</h1>
 
-            <button type="button">
-              Haal data op!
-            </button>
+            <button type="button">Haal data op!</button>
           </span>
         </div>
 
         {/*CONTENT ------------------ */}
         <div className="weather-content">
-          <TabBarMenu/>
+          <TabBarMenu />
 
           <div className="tab-wrapper">
             Alle inhoud van de tabbladen komt hier!
           </div>
         </div>
 
-        <MetricSlider/>
+        <MetricSlider />
       </div>
     </>
   );
