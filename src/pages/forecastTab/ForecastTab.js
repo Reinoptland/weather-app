3f1ed089d33a154dd5f709e0ede6a60b1e3227b5
Reinoptland <reinoptland@gmail.com>
2021-04-07 15:05:25 +0200

    Add effect to forecast tab, run effect when coordinates change

--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import "./ForecastTab.css";
 
 function ForecastTab(props) {
-  console.log("PROPS IN FORECAST:", props);
+  console.log("PROPS IN FORECAST:", props.coordinates);
+
+  useEffect(() => {
+    console.log("EFFECT IN FORECAST!");
+  }, [props.coordinates]);
+
   return (
     <div className="tab-wrapper">
       <article className="forecast-day">
