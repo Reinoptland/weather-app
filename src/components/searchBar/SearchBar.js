e58a58c1d227ca9a565be01d974db24db6478ab4
Reinoptland <reinoptland@gmail.com>
2021-04-07 14:20:43 +0200

    Make input in searchbar controlled component (using state)

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './SearchBar.css';
+import React, { useState } from "react";
+import "./SearchBar.css";
 
 function SearchBar() {
+  const [query, setQuery] = useState("cheese");
   return (
     <span className="searchbar">
       <input
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
         type="text"
         name="search"
         placeholder="Zoek een stad in Nederland"
       />
 
-      <button type="button">
-        Zoek
-      </button>
+      <button type="button">Zoek</button>
     </span>
   );
-};
+}
 
 export default SearchBar;
